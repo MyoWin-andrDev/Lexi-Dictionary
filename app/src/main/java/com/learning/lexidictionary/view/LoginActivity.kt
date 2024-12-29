0767866565bae1cee6095c302bac45747009f42b
Phyo Hein ( Ezekiel ) <mwrcps11@gmail.com>
2024-12-29 21:27:06 +0700

    Setting login form

---
+++ app/src/main/java/com/learning/lexidictionary/view/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.learning.lexidictionary.view
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.learning.lexidictionary.R
+import com.learning.lexidictionary.databinding.ActivityIntroBinding
+import com.learning.lexidictionary.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
