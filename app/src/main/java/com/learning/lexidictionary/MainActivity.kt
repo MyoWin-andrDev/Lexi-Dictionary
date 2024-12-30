3a534f812934d4e2a37ac13c500c5c74e5b15e1b
Phyo Hein ( Ezekiel ) <mwrcps11@gmail.com>
2024-12-30 15:56:55 +0700

    Fetching Data from API

--- app/src/main/java/com/learning/lexidictionary/MainActivity.kt
+++ app/src/main/java/com/learning/lexidictionary/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.learning.lexidictionary
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.learning.lexidictionary.apiService.DictionaryAPI
 import com.learning.lexidictionary.databinding.ActivityMainBinding
+import com.learning.lexidictionary.model.DefinationData
+import com.learning.lexidictionary.model.DefinationDataItem
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
@@ -14,6 +24,33 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.searchBar.
+        LoadDefination()
+    }
+
+    fun LoadDefination(){
+        //Creating Retrofit Instance
+        val retrofit  = Retrofit.Builder()
+            .baseUrl("https://dictionaryapi.com/api/v3/references/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val apiService = retrofit.create(DictionaryAPI::class.java)
+        val call = apiService.getEntry("apple")
+        call.enqueue(object : Callback<DefinationData>{
+            override fun onResponse(
+                call: Call<DefinationData>,
+                response: Response<DefinationData>
+            ) {
+               // TODO("Not yet implemented")
+                if(response.isSuccessful){
+                    val word = response.body()
+                    Log.d("word",word.toString())
+                }
+            }
+
+            override fun onFailure(call: Call<DefinationData>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
--- app/src/main/java/com/learning/lexidictionary/apiService/DictionaryAPI.kt
+++ app/src/main/java/com/learning/lexidictionary/apiService/DictionaryAPI.kt
@@ -1,13 +1,16 @@
 package com.learning.lexidictionary.apiService
 
+import com.learning.lexidictionary.model.DefinationData
 import com.learning.lexidictionary.model.DefinationDataItem
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
-interface DictionaryAPI {
-    @GET("sd4/json")
+interface DictionaryAPI{
+    @GET("sd4/json/{word}")
     fun getEntry(
+        @Path("word") word : String,
         @Query("key") key : String = "d6b440b8-391a-44b8-8889-c080bc87d68a"
-    ) : Call<DefinationDataItem>
+    ) : Call<DefinationData>
 }
