7c2ddba5ce4dc54404dfdc9f42df94d85b48abff
Phyo Hein ( Ezekiel ) <mwrcps11@gmail.com>
2025-02-19 20:14:50 +0700

    Modified definition_layout.xml

--- app/src/main/java/com/learning/lexidictionary/adapter/AdditionalAdapter.kt
+++ app/src/main/java/com/learning/lexidictionary/adapter/AdditionalAdapter.kt
@@ -5,16 +5,20 @@ import android.os.Build
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.learning.lexidictionary.data.Definition
+import com.learning.lexidictionary.databinding.ActivityDefinationBinding
 import com.learning.lexidictionary.databinding.AdditionalLayoutBinding
 import com.learning.lexidictionary.model.learnerEdition.LearnerDataItem
 import com.learning.lexidictionary.handler.DefinitionHandler
+import com.learning.lexidictionary.view.DefinitionActivity
 
 @RequiresApi(Build.VERSION_CODES.O)
 class AdditionalAdapter(val context : Context, val learnerItemList : List<LearnerDataItem>, val wordId : String) : RecyclerView.Adapter<AdditionalAdapter.AdditionalViewHolder>() {
     private val defClass = Definition()
     private val defHandlerClass = DefinitionHandler(context, wordId)
+    private lateinit var defBinding : ActivityDefinationBinding
     class AdditionalViewHolder (val binding : AdditionalLayoutBinding) : RecyclerView.ViewHolder(binding.root){
     }
 
@@ -33,9 +37,14 @@ class AdditionalAdapter(val context : Context, val learnerItemList : List<Learne
     override fun onBindViewHolder(holder: AdditionalViewHolder, position: Int) {
         //TODO("Not yet implemented")
         val learnerItem = learnerItemList[position]
-        val drosList = learnerItemList[0].dros.indices
+        val drosList = learnerItemList[0].dros?.indices
+        if(drosList != null){
         for(i in drosList) {
             defHandlerClass.handlePhraseAndUsage(learnerItemList, i, holder)
+            }
+        }
+        else{
+            holder.binding.additionalLayout.isVisible = false
         }
     }
 }
--- app/src/main/java/com/learning/lexidictionary/apiService/DictionaryService.kt
+++ app/src/main/java/com/learning/lexidictionary/apiService/DictionaryService.kt
@@ -15,20 +15,20 @@ interface DictionaryService{
     @GET("learners/json/{query}")
     fun getLearnerResult (
         @Path("query") query: String,
-        @Query("key") key : String = "636fd0e7-95e2-476a-8495-eee2f107cf16"
+        @Query("key") key : String = "Your API Key"
     ) : Call<LearnerData>
 
     //StudentEdition
     @GET("/sd4/json/{query}")
     fun getStudentResult (
         @Path("query") query : String,
-        @Query("key") key : String = "d6b440b8-391a-44b8-8889-c080bc87d68a"
+        @Query("key") key : String = "Your API Key"
     ) : Call<StudentData>
 
     //Thesaurus
     @GET("thesaurus")
     fun getThesaurusResult(
         @Query("word") word : String,
-        @Header("x-api-key") api : String = "3rQJb6U2OTs6SyYCSvq+Mg==7lhwFHVuH52NjQP6"
+        @Header("x-api-key") api : String = "Your API Key"
     ) : Call<Thesaurus>
 }
